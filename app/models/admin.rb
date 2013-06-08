a8ca2c810062b45a01036d77cacc9124c006d57b
Andrew Stamm <andrewstamm@me.com>
2013-06-08 09:46:43 -0500

    initial tests setup

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,7 +3,7 @@ class Admin < ActiveRecord::Base
   has_secure_password
 
   validates :first_name, :last_name, :email, :password_digest, :presence => true
-  validates :email, :format => {:with =>  /\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3}/},
+  validates :email, :format => {:with =>  /\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3}/}
 
   def full_name
     self.first_name + self.last_name
