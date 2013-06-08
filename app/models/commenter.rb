fffb5f3a99c8933b35c1bbd827c6dbfb4263208f
Andrew Stamm <andrewstamm@me.com>
2013-06-07 22:14:33 -0500

    admin and user models

--- app/models/commenter.rb
+++ app/models/commenter.rb
@@ -1,3 +1,17 @@
 class Commenter < ActiveRecord::Base
-  # attr_accessible :title, :body
+
+  has_secure_password
+
+  attr_accessible :email, :password, :first_name, :last_name
+
+  before_create   { |user| user.email = user.email.downcase }
+
+  validates :first_name, :last_name, :email, :password_digest, :presence => true
+  validates :email, :format => {:with =>  /\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3}/}
+  validates :email, :uniqueness => true
+
+  def full_name
+    self.first_name + self.last_name
+  end
+
 end
