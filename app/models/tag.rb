328837d7cea15320cdbcd2f988a744156a695bf1
Andrew Stamm <andrewstamm@me.com>
2013-06-08 10:42:48 -0500

    tag tests written/passed

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ActiveRecord::Base
-  # attr_accessible :title, :body
+
+  attr_accessible :tag, :tagable_id, :tagable_type
+
+  validates :tag, :tagable_id, :tagable_type, :presence => true
+
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,4 +2,24 @@ require 'spec_helper'
 
 describe Tag do
 
+  let(:photo) { Photo.new(:id => 1) }
+
+  it "should require a tag" do
+    tag = Tag.create( :tagable_id   => "1",
+                      :tagable_type => "Post")
+    tag.should_not be_valid
+  end
+
+  it "should require a tagable_id" do
+    tag = Tag.create( :tag          => "butter",
+                      :tagable_type => "Post")
+    tag.should_not be_valid
+  end
+
+  it "should require a tagable_type" do
+    tag = Tag.create( :tag          => "butter",
+                      :tagable_id   => "1")
+    tag.should_not be_valid
+  end
+
 end
