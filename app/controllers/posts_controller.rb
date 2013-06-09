5375e16b6853e96ae8ddd9eabab0768b7850da93
Andrew Stamm <andrewstamm@me.com>
2013-06-09 11:24:11 -0500

    prev/next links changed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
 
   def index
     @title = "DevSquirrel Blog"
-    @posts = Post.paginate(:page => params[:page], :order => 'created_at DESC')
+    @posts = Post.paginate( :page => params[:page],
+                            :order => 'created_at DESC')
   end
 
   def show
