c5008493d3822b9cea416ceaf34cc827afeacec2
Andrew Stamm <andrewstamm@me.com>
2013-06-22 13:08:59 -0500

    set precompile to false for compass/sass

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module DevSquirrel
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -61,6 +61,7 @@ module DevSquirrel
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
