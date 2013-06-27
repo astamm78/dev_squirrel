2052c80a1c2ef4a065ab1360e6c21523edaef365
Andrew Stamm <andrewstamm@me.com>
2013-06-27 10:17:23 -0500

    thumbnails to photo page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,12 +3,17 @@ class PhotosController < ApplicationController
   def index
     @title = "Photos"
     @body_class = "photo"
-    @photo = Photo.first
+    @photos = Photo.all
   end
 
   def create
     photo = Photo.create(params[:photo]);
-    redirect_to photos_path
+    if photo.save
+      redirect_to photos_path
+    else
+      flash[:errors] = "Photo did not save"
+      redirect_to admins_path
+    end
   end
 
 end
