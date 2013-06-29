886b41d3a80072f4b82537dad9ba4bdef2756dab
Andrew Stamm <andrewstamm@me.com>
2013-06-29 16:33:10 -0500

    finished admin page

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('.admin_post_form').toggle();
+  $('.admin_photo_form').toggle();
+
+  $('#new_content a').click(function(event) {
+    event.preventDefault();
+    var target = $(this).attr('data');
+    $('.entry').hide();
+    $(target).toggle();
+    $('#new_content').toggle();
+  });
+
+  $('.container').on('click', 'a.admin_close', function(event) {
+    event.preventDefault();
+    $(this).closest('.entry').toggle();
+    $('#new_content').toggle();
+  });
+
+});
