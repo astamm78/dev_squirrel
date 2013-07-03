e92ac4c85a34439009b2e6074a6ad6eb01ff80bf
Andrew Stamm <andrewstamm@me.com>
2013-07-03 13:43:37 -0500

    Added posted on date, and post excerpts to tag display page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     @_current_user ||= session[:user_id] && Admin.find_by_id(session[:user_id])
   end
 
+  def smart_truncate(s, opts = {})
+    opts = {:words => 12}.merge(opts)
+    if opts[:sentences]
+      return s.split(/\.(\s|$)+/)[0, opts[:sentences]].map{|s| s.strip}.join('. ') + '.'
+    end
+    a = s.split(/\s/) # or /[ ]+/ to only split on spaces
+    n = opts[:words]
+    a[0...n].join(' ') + (a.size > n ? '...' : '')
+  end
+
 end
