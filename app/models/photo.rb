6632c3d91ccd1f12823625eeeafb0869303c4d2f
Andrew Stamm <andrewstamm@me.com>
2013-07-04 10:15:50 -0500

    Photo exif data display and creations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,10 +8,8 @@ class Photo < ActiveRecord::Base
   validates :image, :presence => true
 
   has_attached_file :image, styles: {
-    thumb:    '100x100>',
-    square:   '200x200>',
-    medium:   '300x300>',
-    large:    '600x600>'
+    thumb:    '200x200>',
+    large:    '920x920>'
   }
 
   after_image_post_process :copy_exif_data
@@ -27,10 +25,12 @@ class Photo < ActiveRecord::Base
   private
   def copy_exif_data
     exif =EXIFR::JPEG.new(self.image.queued_for_write[:original].path)
+    p exif
     self.camera     = exif.model
-    self.taken_at   = exif.date_time
+    self.taken_at   = exif.date_time_original
     self.exposure   = exif.exposure_time.to_s
     self.aperture   = exif.f_number.to_f
+    self.focal      = exif.focal_length.to_s.gsub(/\/1/, "")
   end
 
 end
