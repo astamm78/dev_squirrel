b0d14e1a9d476b8b68804d746064182fbb44f85e
Andrew Stamm <andrewstamm@me.com>
2013-07-21 11:03:54 -0500

    Photo AJAX fix

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 
-  $('.full_size').toggle();
-
-  $('.container').on('click', 'a.photo_thumb', function(event) {
+  $('.main_box').on('click', 'a.photo_thumb', function(event) {
     event.preventDefault();
     var url = $(this).attr('href');
     $('.full_size').load(url).removeClass('hidden');
   });
 
-  $('.container').on('click', '.image_close', function(event) {
+  $('.main_box').on('click', '.image_close', function(event) {
     event.preventDefault();
     $('.full_size').addClass('hidden').html('');
   });
 
-  $('.container').on('click', 'a.photo_nav', function(event){
+  $('.main_box').on('click', 'a.photo_nav', function(event){
     event.preventDefault();
     var nextImage = $(this).attr('href');
     $('.full_size').load(nextImage);
