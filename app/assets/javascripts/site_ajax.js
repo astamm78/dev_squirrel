3a3a881597ac6264a1ddaef7053110e881474352
Andrew Stamm <andrewstamm@me.com>
2013-07-21 10:59:50 -0500

    Disabled AJAX for new css layout

--- app/assets/javascripts/site_ajax.js
+++ app/assets/javascripts/site_ajax.js
@@ -1,27 +1,27 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  // Nav Link AJAX
-  $('.container').on('click', 'a.jlink, .pagination a', function(event) {
-    event.preventDefault();
-    $('.styled_box').html('<img style="width:550px;height:400px;padding:0 140px;margin-top:-60px;" src="/assets/loadingAnimation.gif"/>');
-    if ($(this).attr('data') === undefined) {
-      var bodyClass = "posts";
-    }
-    else {
-      var bodyClass = $(this).attr('data');
-    }
-    var url = $(this).attr('href');
-    history.pushState({ url: url, bodyClass: bodyClass }, '', url);
-    $('body').removeClass();
-    $('.container').load(url + ' .container');
-    $('body').addClass(bodyClass);
-    $('html, body').animate({ scrollTop: 0 }, 0);
-  });
+//   // Nav Link AJAX
+//   $('.main').on('click', 'a.jlink, .pagination a', function(event) {
+//     event.preventDefault();
+//     $('#content').html('<img style="width:550px;height:400px;padding:0 140px;margin-top:-60px;" src="/assets/loadingAnimation.gif"/>');
+//     if ($(this).attr('data') === undefined) {
+//       var bodyClass = "posts";
+//     }
+//     else {
+//       var bodyClass = $(this).attr('data');
+//     }
+//     var url = $(this).attr('href');
+//     history.pushState({ url: url, bodyClass: bodyClass }, '', url);
+//     $('body').removeClass();
+//     $('.main').load(url + ' .main');
+//     $('body').addClass(bodyClass);
+//     $('html, body').animate({ scrollTop: 0 }, 0);
+//   });
 
-  // For Browser 'Back' Button
-  window.onpopstate = function(event) {
-    $('.container').load(event.state.url + ' .container');
-    $('body').removeClass().addClass(event.state.bodyClass);
-  };
+//   // For Browser 'Back' Button
+//   window.onpopstate = function(event) {
+//     $('.main').load(event.state.url + ' .main');
+//     $('body').removeClass().addClass(event.state.bodyClass);
+//   };
 
-});
+// });
